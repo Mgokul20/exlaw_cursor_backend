58522f6aab113ca70f4b8a0c4eb2826ace75e7cc
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-10-12 01:02:52 +0530

    oush

--- api/index.js
+++ api/index.js
@@ -1,14 +1,35 @@
 // Requiring modules
 const express = require('express');
-var MongoClient = require('mongodb').MongoClient;
+// require('dotenv').config()
+var {MongoClient,Timestamp } = require('mongodb');
+var url = "mongodb+srv://Dhanush:SD18A2004@cluster0.2s94ek1.mongodb.net/?retryWrites=true&w=majority";
 const app = express();
-const config = {
-	user: 'root',
-	password: 'root',
-	server: 'localhost',
-	database: 'exlaw'
-};
-app.get('/', function (req, res) {
-
+const mongoClient = new MongoClient(url);
+app.get('/', async function (req, res) {
+	const database = (await mongoClient).db("mini_project");
+	const collection =await database.collection("EXLAW");
+	 const resul = await collection.find({}).toArray( function(err, result) {
+		if (err) throw err;
+		db.close();
+		console.log(result);
+		return result
+		// console.log(result.name);
+		
+	  })
+	 console.log(resul)
 });
+app.post('/post', async function (req, res) {
+	// let body=req.body;
+	try{
+		const database = (await mongoClient).db("mini_project");
+	const collection =await database.collection("EXLAW");
+	const results = await collection.insertOne(req.body);
+    res.send(JSON.stringify(results));
+    }catch(err){
+      res.send({statusCode: 500, body: err.toString()});
+      }
+		});
 
+
+const port =5000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
